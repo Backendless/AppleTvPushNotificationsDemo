2bf52584187752617e9100d3ce998ef0843edf6a
Olga Danylova <olga@themidnightcoders.com>
2018-06-25 10:15:00 +0300

    Initial Commit

---
+++ AppleTvPushNotificationsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleTvPushNotificationsDemo
+//
+//  Created by Olga Danylova on /25/618.
+//  Copyright © 2018 Olga Danylova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
